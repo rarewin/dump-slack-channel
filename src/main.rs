cfbeba78119e967f60d2fe2e94752d8ea0a364c6
Katsuki Kobayashi <katsuki.kobayashi@xacti-co.com>
2022-07-21 08:25:03 +0900

    get channel list

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use std::collections::HashMap;
+
+use anyhow::Result;
+use clap::Parser;
+use serde::Deserialize;
+
+#[derive(Parser, Debug)]
+#[clap(version)]
+struct Arg {
+    #[clap(short, long, value_parser)]
+    token: String,
+}
+
+fn main() -> Result<()> {
+    let args = Arg::parse();
+
+    let client = reqwest::blocking::Client::new();
+    let mut params = HashMap::new();
+    params.insert("token", args.token);
+
+    let response = client
+        .post("https://slack.com/api/conversations.list")
+        .form(&params)
+        .send()?;
+
+    println!("{}", response.text()?);
+
+    Ok(())
 }
